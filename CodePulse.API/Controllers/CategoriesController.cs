f9d8a8cb8113dd306a49418ce95f36a56f931d57
Ritesh <ritesh.sargam00@gmail.com>
2025-04-08 12:44:51 +0530

    Creating New Controller (CategoriesController)

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    // https://localhost:portnumber/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
