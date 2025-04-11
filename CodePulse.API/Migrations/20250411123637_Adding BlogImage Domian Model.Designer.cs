691559bf10910f888fe6eb92da7330d8408675bb
Ritesh <ritesh.sargam00@gmail.com>
2025-04-11 18:08:12 +0530

    Image Functionality Domain Model Changes

--- CodePulse.API/Migrations/20250411123637_Adding BlogImage Domian Model.Designer.cs
+++ CodePulse.API/Migrations/20250411123637_Adding BlogImage Domian Model.Designer.cs
@@ -4,6 +4,7 @@ using CodePulse.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodePulse.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250411123637_Adding BlogImage Domian Model")]
+    partial class AddingBlogImageDomianModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
